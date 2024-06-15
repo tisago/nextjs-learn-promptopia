1c30afb3be925f52dc470ce2053d2a5f22c9f403
santiago <tisagrid@gmail.com>
2024-06-15 18:42:26 -0300

    Suspense is not defined

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Suspense } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 
 import Form from '@components/form'
